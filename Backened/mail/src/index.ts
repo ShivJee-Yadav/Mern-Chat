843068ca94ba47a4c6223d36e74ba0da82cee10f
ShivJee-Yadav <shivyadav1936@gmail.com>
2026-02-05 18:46:49 +0530

    consumer.ts - OTP configuration && mailController

--- Backened/mail/src/index.ts
+++ Backened/mail/src/index.ts
@@ -1,8 +1,11 @@
 import express from 'express';
-import dotenv from 'dotenv'
-const app = express()
+import dotenv from 'dotenv';
 
 dotenv.config();
+const app = express();
+
+console.log(process.env.PORT);
+
 app.listen(process.env.PORT, ()=>{
-    console.log(`server is running on port ${process.env.PORT}`)
+    console.log(`server is running on psort ${process.env.PORT}`)
 })
--- Backened/user/src/controllers/user.ts
+++ Backened/user/src/controllers/user.ts
@@ -1,1 +1,37 @@
+import { publishToQueue } from "../config/rabbitmq.js";
+import TryCatch from "../config/TryCatch.js";
+import { redisClient } from "../index.js";
+
 //  Rabit MQ or kafka 
+export const loginUser = TryCatch(async(req,res)=>{
+    const {email} = req.body
+    const rateLimitKey = `otp:ratelimit:${email}`
+    const rateLimit = await redisClient.get(rateLimitKey)
+    if(rateLimit)
+    {
+        res.status(429).json({
+            message: "Too Many request. Please Wait before Requesting new OPT",
+        });
+        return;
+    }
+    const otp = Math.floor(100000 + Math.random() * 900000).toString()
+
+    const otpkey = `opt:${email}`
+    await redisClient.set(otpkey,otp,{
+        EX:300,
+    });
+    await redisClient.set(rateLimitKey,"true",{
+        EX:60,
+    });
+    const message = {
+        to: email,
+        subject:"your OTP code",
+        body:`Your OTP is ${otp}. It is valid for 5 Minutes`,
+    };
+    await publishToQueue("send-otp",message)
+
+    res.status(200).json({
+        message:"OTP sent yo your mail"
+    })
+
+})
